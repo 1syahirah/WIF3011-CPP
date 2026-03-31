6c5e4034507536e7da624290e6e11342c2f52a75
1syahirah <22000741@siswa.um.edu.my>
2026-03-31 10:39:35 +0800

    L2Q3

--- src/Main.java
+++ src/Main.java
@@ -8,26 +8,49 @@ import java.util.Random;
 
 public class Main  {
     public static void main(String[] args) throws Exception {
-         Random rd = new Random();
+          Random rd = new Random();
 
          int[] arr = rd.ints(1000000,1,50001).toArray();
         
-        int mid = arr.length/2;
+         Timer timer = new Timer();
+         timer.start();
 
-        FindMax task1 = new FindMax(arr, 0, mid);
-        FindMax task2 = new FindMax(arr, mid, arr.length);
+         int largest = arr[0];
+         
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] > largest){
+                largest = arr[i];
+            }
+        }
 
+        timer.stop();
+
+         System.out.println("Sequential largest num: " + largest);
+         System.out.println("Time taken for sequential: " + timer.getTime() + " ms");
+//---------------------------------------------------------------------------------------------------
+
+         int mid = arr.length/2;
+
+         FindMax task1 = new FindMax(arr, 0, mid);
+         FindMax task2 = new FindMax(arr, mid, arr.length);
+         
          Thread t1 = new Thread(task1);
          Thread t2 = new Thread(task2);
 
-         t1.start();
-         t2.start();
+         Timer timerThread = new Timer();
+        timerThread.start();
 
-        t1.join();
-        t2.join();
+        t1.start();
+        t2.start();
 
-        int largest = Math.max(task1.getLocalMax(), task2.getLocalMax());
+         t1.join();
+         t2.join();
 
-        System.out.println("Largest number: " + largest);
+         timerThread.stop();
+         
+         int largestThread = Math.max(task1.getLocalMax(), task2.getLocalMax());
+         System.out.println("Threaded Largest: " + largestThread);
+        System.out.println("Time taken for thread: " + timerThread.getTime() + " ms");
+    
     }
 }
