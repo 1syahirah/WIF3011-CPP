7e5b6b4f605fbf5638b26dea9378a277eb97751f
1syahirah <22000741@siswa.um.edu.my>
2026-04-27 21:55:30 +0800

    delete unused files

--- src/SharedData.java
+++
@@ -1,5 +0,0 @@
-public class SharedData {
-   // The volatile keyword guarantees memory visibility across threads.
-    // It ensures the CheckTask instantly sees updates made by the IncrementTask.
-    public volatile int counter = 0; 
-}
