a32c0630b5fde3eb7bf5d9ad987644ef7ebc1fe3
1syahirah <22000741@siswa.um.edu.my>
2026-04-27 21:44:49 +0800

    delete unused file

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,7 @@
-import java.util.Random;
+
 
 public class Main  {
     public static void main(String[] args) throws Exception {
-      BankingQueue queue = new BankingQueue();
-        Random random = new Random();
-
-        // Assign two random, distinct numbers between 1 and 12
-        int wait1 = 1 + random.nextInt(12);
-        int wait2;
-        do {
-            wait2 = 1 + random.nextInt(12);
-        } while (wait1 == wait2); 
-
-        System.out.println("Numbers to wait: " + wait1 + " " + wait2);
-
-        // Initialize threads
-        Thread arrivingThread = new Thread(new ArrivingCustomer(queue));
-        Thread callingThread = new Thread(new CallingQueue(queue));
-        Thread cust1Thread = new Thread(new CustomerInLine(queue, wait1));
-        Thread cust2Thread = new Thread(new CustomerInLine(queue, wait2));
-
-        // Start tasks
-        callingThread.start(); // Start calling first to potentially trigger "No customer to call"
-        arrivingThread.start();
-        cust1Thread.start();
-        cust2Thread.start();
+     
     }
 }
