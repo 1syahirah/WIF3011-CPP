5f73ef81e4a5f916df0ca82abd26063e49c2973a
1syahirah <22000741@siswa.um.edu.my>
2026-07-02 16:50:33 +0800

    delete unused files

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,6 @@
 
 public class Main  {
     public static void main(String[] args){
-        int a = 10;
-        int b = 5;
- 
-        // Each lambda below implements MathOperation's single abstract
-        // method, operation(int, int), with a different calculation.
-        MathOperation addition = (x, y) -> x + y;
-        MathOperation subtraction = (x, y) -> x - y;
-        MathOperation multiplication = (x, y) -> x * y;
-        MathOperation division = (x, y) -> x / y;
- 
-        System.out.println(a + " + " + b + " = " + addition.operation(a, b));
-        System.out.println(a + " - " + b + " = " + subtraction.operation(a, b));
-        System.out.println(a + " * " + b + " = " + multiplication.operation(a, b));
-        System.out.println(a + " / " + b + " = " + division.operation(a, b));
+       
     }
 }
