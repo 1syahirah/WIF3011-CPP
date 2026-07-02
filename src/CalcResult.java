9295c04fe49c7430ee47c061a19deed81b097e9b
1syahirah <22000741@siswa.um.edu.my>
2026-07-02 16:29:26 +0800

    fix things

---
+++ src/CalcResult.java
@@ -0,0 +1,11 @@
+public class CalcResult {
+    final int n;
+    final long fibValue;
+    final Timer timer;
+
+    public CalcResult(int n, long fibValue, Timer timer) {
+        this.n = n;
+        this.fibValue = fibValue;
+        this.timer = timer;
+    }
+}
